07e9f2a5828db20ca7475192f96c7b638d4f8143
Benjamin Scherer <ThatsNoMoon.Alderaan@gmail.com>
2020-04-01 20:19:57 -0600

    improve and format build.rs
    Report an error to the user when the extension of a file in the shaders
    directory was nonexistent or unexpected. Indented using tabs.

--- build.rs
+++ build.rs
@@ -1,54 +1,52 @@
 use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    println!("cargo:rerun-if-changed=assets/shaders");
+	println!("cargo:rerun-if-changed=assets/shaders");
 
-    fs::create_dir_all("target/shaders/")?;
+	fs::create_dir_all("target/shaders/")?;
 
-    let mut compiler = shaderc::Compiler::new().unwrap();
-    
-    for entry in fs::read_dir("assets/shaders")? {
-        let entry = entry?;
+	let mut compiler = shaderc::Compiler::new().unwrap();
+	
+	for entry in fs::read_dir("assets/shaders")? {
+		let entry = entry?;
 
-        if entry.file_type()?.is_file() {
-            let input_path = entry.path();
+		if entry.file_type()?.is_file() {
+			let input_path = entry.path();
 
-            let input_name: String = entry.file_name().to_string_lossy().into_owned();
+			let input_name: String = entry.file_name().to_string_lossy().into_owned();
 
-            let shader_kind = input_path.extension().and_then(|ext| {
-                match ext.to_string_lossy().as_ref() {
-                    "vert" => Some(shaderc::ShaderKind::Vertex),
-                    "frag" => Some(shaderc::ShaderKind::Fragment),
-                    _ => None,
-                }
-            });
+			let shader_kind = input_path.extension().and_then(|ext| {
+				match ext.to_string_lossy().as_ref() {
+					"vert" => Some(shaderc::ShaderKind::Vertex),
+					"frag" => Some(shaderc::ShaderKind::Fragment),
+					_ => None,
+				}
+			}).ok_or_else(|| format!("Could not identify shader type of {:?}; expected .vert or .frag extension", input_path))?;
 
-            if let Some(shader_kind) = shader_kind {
-                let source = fs::read_to_string(&input_path)?;
-                let artifact = compiler.compile_into_spirv(
-                    &source,
-                    shader_kind,
-                    &input_name,
-                    "main",
-                    None,
-                )?;
+			let source = fs::read_to_string(&input_path)?;
+			let artifact = compiler.compile_into_spirv(
+				&source,
+				shader_kind,
+				&input_name,
+				"main",
+				None,
+			)?;
 
-                if artifact.get_num_warnings() > 0 {
-                    eprintln!(
-                        "Shader compilation warning: {}: {}",
-                        input_name,
-                        artifact.get_warning_messages());
-                }
+			if artifact.get_num_warnings() > 0 {
+				eprintln!(
+					"Shader compilation warning: {}: {}",
+					input_name,
+					artifact.get_warning_messages());
+			}
 
-                let output_path = format!(
-                    "target/shaders/{}.spv",
-                    input_name,
-                );
+			let output_path = format!(
+				"target/shaders/{}.spv",
+				input_name,
+			);
 
-                fs::write(&output_path, artifact.as_binary_u8())?;
-            }
-        }
-    }
+			fs::write(&output_path, artifact.as_binary_u8())?;
+		}
+	}
 
-    Ok(())
+	Ok(())
 }
